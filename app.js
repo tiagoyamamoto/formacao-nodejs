ce141c3027a968e5f3953244f373c9f10c51faff
Tiago Yamamoto <japa@rede5.com.br>
2022-02-22 15:28:32 -0300

    Seu primeiro código com Node.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com"
+
+console.log("Hello World!");
+
+
+if (mostrarSite) { // se mostrar site
+    console.log(site); // print o site
+}
