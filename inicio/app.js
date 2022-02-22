27a13e255cbf1b0ecd0c8de78d0d7767e10d6cf0
Tiago Yamamoto <japa@rede5.com.br>
2022-02-22 16:00:32 -0300

    Modulos

---
+++ inicio/app.js
@@ -0,0 +1,6 @@
+var calculadora = require("./calculadora");
+
+console.log(calculadora.mult(10,20));
+console.log(calculadora.soma(20,40))
+calculadora.nome = "Calculadora do Victor!"
+console.log(calculadora.nome);
