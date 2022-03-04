577ba2abc0b5b3b57f35aa0e729ad1b45c2e0141
Tiago Yamamoto <japa@rede5.com.br>
2022-03-04 12:49:15 -0300

    Rotas

--- express/index.js
+++ express/index.js
@@ -1,6 +1,18 @@
 const express = require("express"); // Importando o express
 const app = express(); // Iniciando o express
 
+app.get("/", function (req, res) {
+    res.send("Bem vindo ao guia do programador");
+});
+
+app.get("/blog", function (req, res) {
+    res.send("Bem vindo ao meu blog!")
+})
+
+app.get("/canal/youtube", function (req, res) {
+    res.send("<h1>Bem vindo ao meu canal!</h1>")
+})
+
 app.listen(4000, function (erro) {
     if (erro) {
         console.log("Ocorreu um erro!");
