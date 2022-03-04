2285d44be1e756c3ba75001a0a5e18c5c378b602
Tiago Yamamoto <japa@rede5.com.br>
2022-03-04 17:11:34 -0300

    Query params

--- express/index.js
+++ express/index.js
@@ -17,7 +17,12 @@ app.get("/blog/:artigo?", function (req, res) {
 })
 
 app.get("/canal/youtube", function (req, res) {
-    res.send("<h1>Bem vindo ao meu canal!</h1>")
+    var canal = req.query["canal"];
+    if (canal) {
+        res.send(canal)
+    } else {
+        res.send("Nenhum canal fornecido!")
+    }
 })
 
 app.get("/ola/:nome/:empresa", function (req, res) {
