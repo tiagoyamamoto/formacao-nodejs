e24ce8631071130deb8accb3a8af178651480ba0
Tiago Yamamoto <japa@rede5.com.br>
2022-03-09 10:43:32 -0300

    Estruturas condicionais com EJS

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -6,12 +6,14 @@ app.set('view engine', 'ejs')
 
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
-    var lang = req.params.lang
+    var lang = req.params.lang;
+    var exibirMsg = false;
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
-        inscritos: 8000
+        inscritos: 8000,
+        msg: exibirMsg
     });
 });
 
